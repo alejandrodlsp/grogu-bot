3636bdbf82cd7fec710a98deace7a610de71cc5f
alejandro.santos <alejandro.santos@longboat.com>
2021-03-13 11:30:22 +0000

    Project .env and discord setup

--- src/client.py
+++ src/client.py
@@ -4,11 +4,16 @@ import os
 from src.logger import log
 
 class Client:
-    def __init__(self, name):
+    def __init__(self):
         COMMAND_PREFIX = os.getenv("COMMAND_PREFIX")
-        self.bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+        DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+        client = commands.Bot(command_prefix=COMMAND_PREFIX)
 
         @client.event
         async def on_ready():
             log("Bot is ready")
-        
+
+        @client.event
+        async def on_member_join(member):
+
+        client.run(DISCORD_TOKEN)
